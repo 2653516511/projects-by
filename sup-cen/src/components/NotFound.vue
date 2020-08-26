44bef4e91058b75f5b5aad9926488ed7b402607a
zjiaoyan <33372197+2653516511@users.noreply.github.com>
2020-08-26 21:43:46 +0800

    Update NotFound.vue

--- sup-cen/src/components/NotFound.vue
+++ sup-cen/src/components/NotFound.vue
@@ -7,6 +7,8 @@
       Check your spelling or return to the home page.
     </p>
     <div class="actions">
+      <!-- 也可使用路由展位，在router中注册 -->
+      <!-- <router-view></router-view> -->
       <router-link tag="button" :to="{name: 'home'}">Return to home</router-link>
     </div>
   </main>
